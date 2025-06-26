5435f66a53ec4aa1c0c4ac2c407d0c5224e79912
ZuidNET <zuidnet944@gmail.com>
2025-06-26 13:08:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ def weather():
     temperature = int(data["main"]["temp"])
     realfeel = int(data["main"]["feels_like"])
     humidity = f'{data["main"]["humidity"]}%'
-    weather_text = data["weather"][0]["description"].title()
-    weather_icon = get_icon_code(data["weather"][0]["icon"])
-    daylight = "True" if data["weather"][0]["icon"].endswith("d") else "False"
+    openweather_icon = data["weather"][0]["icon"]
+    accuweather_icon, accuweather_text = get_icon_and_text(openweather_icon)
+    daylight = "True" if openweather_icon.endswith("d") else "False"
 
     xml = f"""<?xml version="1.0" encoding="UTF-8"?>
 <weather>
@@ -32,29 +32,37 @@ def weather():
 <realfeel>{realfeel}</realfeel>
 <unit>C</unit>
 <humidity>{humidity}</humidity>
-<weathertext>{weather_text}</weathertext>
-<weathericon>{weather_icon}</weathericon>
+<weathertext>{accuweather_text}</weathertext>
+<weathericon>{accuweather_icon}</weathericon>
 </currentconditions>
 </weather>
 """
     return Response(xml, mimetype='application/xml')
 
-def get_icon_code(openweather_icon):
+def get_icon_and_text(openweather_icon):
     mapping = {
-        "01d": "1", "01n": "33",
-        "02d": "2", "02n": "34",
-        "03d": "3", "03n": "35",
-        "04d": "7", "04n": "38",
-        "09d": "12", "09n": "40",
-        "10d": "14", "10n": "39",
-        "11d": "15", "11n": "41",
-        "13d": "22", "13n": "44",
-        "50d": "11", "50n": "11"
+        "01d": ("1", "Sunny"),
+        "01n": ("33", "Clear"),
+        "02d": ("2", "Mostly Sunny"),
+        "02n": ("34", "Mostly Clear"),
+        "03d": ("3", "Partly Sunny"),
+        "03n": ("35", "Partly Cloudy"),
+        "04d": ("7", "Cloudy"),
+        "04n": ("38", "Mostly Cloudy"),
+        "09d": ("12", "Showers"),
+        "09n": ("40", "Mostly Cloudy w/ Showers"),
+        "10d": ("14", "Partly Sunny w/ Showers"),
+        "10n": ("39", "Partly Cloudy w/ Showers"),
+        "11d": ("15", "T-Storms"),
+        "11n": ("41", "Partly Cloudy w/ T-Storms"),
+        "13d": ("22", "Snow"),
+        "13n": ("44", "Mostly Cloudy w/ Snow"),
+        "50d": ("11", "Fog"),
+        "50n": ("11", "Fog")
     }
-    return mapping.get(openweather_icon, "7")  # default: Cloudy
+    return mapping.get(openweather_icon, ("7", "Cloudy"))
 
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
